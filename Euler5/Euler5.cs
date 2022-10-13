c2ffbd455371935eed70860c51cbe2f4d181db8a
LukasMarsch <marschlukas@gmail.com>
2022-10-13 14:05:48 +0200

    added comments and correct solution

--- Euler5/Euler5.cs
+++ Euler5/Euler5.cs
@@ -1,4 +1,11 @@
-﻿static int findSmallestByAllDivisable(int n){
+﻿/* 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+We are going to achieve this by testing the divisability of our number after increasing it. We always have to increase
+it for atleast the highest Divisor we have.
+ */
+static int findSmallestByAllDivisable(int n){
     bool allDivisable = false;
     int num = 0;
     while(!allDivisable){
@@ -12,3 +19,4 @@
     return num;
 }
 Console.WriteLine(findSmallestByAllDivisable(20));
+//correct solution 232792560

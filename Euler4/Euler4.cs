88a8f958e4ec102bf0563a4e5a0bc07766e331e9
LukasMarsch <marschlukas@gmail.com>
2022-10-13 14:10:52 +0200

    added comments and correct answer

--- Euler4/Euler4.cs
+++ Euler4/Euler4.cs
@@ -1,4 +1,12 @@
-﻿using System.Collections;
+﻿/* 
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+
+Here we present the almighty brute-force. If you don't have the brains, you're gonna have to have to computing power.
+Just loop through all numbers and check whether the products are palindromes. store and compare those and find the biggest one, easy?
+ */
+using System.Collections;
 
 static int reverseNumber(int n){
     int reverse = 0;
@@ -34,3 +42,4 @@ static int? numberMixer(){
 }
 
 Console.WriteLine($"largest palindrom product from 2 3-digit factors {numberMixer()}");
+//correct answer 906609

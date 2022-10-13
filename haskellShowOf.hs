66aa67a7b8674d10cfee5ce85198467cbfb6c694
id264136 <p.kranz@ostfalia.de>
2022-10-13 14:43:26 +0200

    removed unnecessary lines

--- haskellShowOf.hs
+++ haskellShowOf.hs
@@ -9,21 +9,14 @@ qst (x:xs) = lower ++ [x] ++ higher
     where lower = qst [a | a <- xs, a <= x]
           higher = qst [a | a <- xs, a > x]
 
-msort :: [Int] -> [Int]
-msort []  = []
-msort [a] = [a]
 msort x   = mmerge ((msort a), (msort b))
     where (a, b) = msplit x
 
-mmerge :: ([Int], [Int]) -> [Int]
 mmerge (x, []) = x
 mmerge ([], y) = y
 mmerge ((x:xs), (y:ys))
     | x < y     = x : (mmerge (xs, (y:ys)))
     | otherwise = y : (mmerge ((x:xs), ys))
 
-msplit :: [Int] -> ([Int], [Int])
-msplit []  = ([], [])
-msplit [a] = ([a], [])
 msplit x   = (take n x, drop n x)
     where n = div (length x) 2

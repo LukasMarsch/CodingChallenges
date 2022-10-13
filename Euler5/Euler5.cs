7c50d296f42cb9be886a9e49dc4ab5d0d6cf0943
LukasMarsch <marschlukas@gmail.com>
2022-10-13 11:28:35 +0200

    added Euler5 kinda brute-force, no comments

---
+++ Euler5/Euler5.cs
@@ -0,0 +1,14 @@
+﻿static int findSmallestByAllDivisable(int n){
+    bool allDivisable = false;
+    int num = 0;
+    while(!allDivisable){
+        num+=20;
+        allDivisable = false;
+        for(int i = n; i>0; i--){
+            allDivisable = (num%i)==0;
+            if(!allDivisable){break;}
+        }
+    }
+    return num;
+}
+Console.WriteLine(findSmallestByAllDivisable(20));

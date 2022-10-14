b7aa98920bfeb890e61c88eda519a4ce6c588c9c
LukasMarsch <marschlukas@gmail.com>
2022-10-14 11:38:09 +0200

    added double datatype and Main, works now

--- Euler6/Euler6.cs
+++ Euler6/Euler6.cs
@@ -12,30 +12,31 @@ Hence the difference between the sum of the squares of the first ten natural num
 
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
  
-This is really just some simple math 
+This is really just some simple math and that's why we're going to use the Math class :D
 */
-
-static int sumOfSquares(int numbers){
-    int sum = 1;
-    for(int count = 0; count >= numbers; count++){
-        sum += (int)Math.Pow((double)count, (double)2);
-        Console.WriteLine(sum);
+using System;
+class compareSquaresAndSums {
+    public static void Main()
+    {
+        int numbers = 100;
+        double sqros = squareOfSums(numbers);
+        double sumos = sumOfSquares(numbers);
+        Console.WriteLine(sqros - sumos);
+        //correct answer is 25164150
     }
-    return sum;
-}
-
-static double squareofSums(int numbers){
-    int sum = 1;
-    for(int count = 0; count >= numbers; count++){
-        sum += count;
+    static double sumOfSquares(int numbers){
+        double sum = 0;
+        for(int count = 0; count <= numbers; count++){
+            sum += Math.Pow(count, 2.0);
+        }
+        return sum;
     }
-    return Math.Pow((double)sum, (double)2);
-}
 
-static double compareSquareSums(int numbers){
-    double resSqoS = squareofSums(numbers);
-    int resSuoS = sumOfSquares(numbers);
-    return resSqoS - resSuoS;
+    static double squareOfSums(int numbers){
+        double sum = 0;
+        for(int count = 1; count <= numbers; count++){
+            sum += count;
+        }
+        return Math.Pow(sum, 2.0);
+    }
 }
-
-Console.WriteLine(compareSquareSums(100));

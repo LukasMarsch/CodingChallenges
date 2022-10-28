157b206d5a1e76b30329ff301a770b616987f03d
Lukas Marsch <marschlukas@gmail.com>
2022-10-29 00:50:41 +0200

    working?

--- Euler8/Euler8.hs
+++ Euler8/Euler8.hs
@@ -1,10 +1,10 @@
-main = do
+{-main = do
     content <- readFile "data.txt"
     print content
    -- print . map readInt . lines $ content
    -- print(parse(lines content))
    -- largestAdj (parse(lines content))
-   -- print(parse(lines content))
+   -- print(parse(lines content))-}
 
 readInt :: [Char] -> Int
 readInt = read
@@ -18,7 +18,14 @@ charToInt [] = []
 charToInt (x:xs) = (read [x]) : charToInt xs
 
 largestAdj :: [Int] -> Int
+largestAdj [] = 0
+largestAdj [x] = x
 largestAdj (x:xs)
-    | x* head xs > maxTail = x * head xs
+    | product (x : (take 12 xs)) > maxTail = product (x : (take 12 xs))
     | otherwise = maxTail
     where maxTail = largestAdj xs
+
+product' :: [Int] -> Int
+product' [] = 1
+product' [x] = x
+product' (x:xs) = x * (product' xs)

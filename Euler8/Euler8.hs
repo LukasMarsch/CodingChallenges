144e414f06cc6648abb22becb0f60e2574e9c4e5
id264136 <p.kranz@ostfalia.de>
2022-10-29 00:08:57 +0200

    added main functionality to split data in an array

--- Euler8/Euler8.hs
+++ Euler8/Euler8.hs
@@ -1,6 +1,15 @@
 main = do
     content <- readFile "data.txt"
     print . map readInt . lines $ content
+    print(parse(lines content))
 
 readInt :: [Char] -> Int
 readInt = read
+
+parse :: [[Char]] -> [Int]
+parse [] = []
+parse (x:xs) = (charToInt x) ++ (parse xs)
+
+charToInt :: [Char] -> [Int]
+charToInt [] = []
+charToInt (x:xs) = (read [x]) : charToInt xs

79169bea95e5aae7730ecadca51e29c0fc0fd68e
LukasMarsch <marschlukas@gmail.com>
2022-11-09 13:31:00 +0100

    added first version of Euler11

---
+++ EulerProject/Euler11/Cursor.cs
@@ -0,0 +1,68 @@
+namespace Euler11
+{
+    class Cursor<Table>
+    {
+        public int position { get; set; }
+
+        public Cursor() {
+            this.position = -1;
+        }
+
+        public int value() => position;
+
+        public int right(){
+            // Zeilenumbruch
+            if(position%20 == 19){
+                throw new IndexOutOfRangeException("Zeilenumbruch");
+            }
+            //nach rechts rutschen
+            return position +=  1;
+        }
+        
+        public int left(){
+            // Zeilenanfang
+            if(position%20 == 0){
+                throw new IndexOutOfRangeException("Zeilenanfang");
+            }
+            return position -= 1;
+        }
+
+        public int left(int a){
+            for(int i = 0; i<a; i++){
+                left();
+            }
+            return position;
+        }
+
+        public int up(){
+            // erste Zeile
+            if(position < 20){
+                throw new IndexOutOfRangeException("Erste Zeile");
+            }
+            return position -= 20;
+        }
+
+        public int down(){
+            //letzte Zeile
+            if(position >= 380){
+                throw new IndexOutOfRangeException("letzte Zeile");
+            }
+            return position += 20;
+        }
+
+        public int rightWithLineBreak()
+        {
+            try {
+                right();
+            } catch {
+                down();
+                try{
+                    return left(19);
+                } catch {
+                    throw new Exception();
+                }
+            }
+            return position;
+        }
+    }
+}
---
+++ EulerProject/Euler11/Program.cs
@@ -0,0 +1,67 @@
+﻿using Euler11;
+
+namespace Euler11
+{
+    class main
+    {
+        public static void Main(String[] args){
+            var t = new Table();
+            Console.WriteLine(t.cursor.position = 19);
+        
+            for(int i=0; i<100;i++){
+            Console.WriteLine(t.cursor.rightWithLineBreak());
+            }
+            
+        }
+    }
+       
+
+/*    class fourAdjacent
+    {
+        public Table table{get; set;}
+
+        public fourAdjacent(Table table)
+        {
+            this.table = table; 
+        }
+
+        public int[] FourInARow()
+        {
+            Cursor<Table> c = new Euler11.Cursor<Table>();
+            c.cursor = table.cursor.value();
+            int[] result = new int[4];
+            for(int i = 0; i<4; i++){
+                    result[i] = table.values[c.right()];
+                }
+            return result;
+        }
+
+        public int[] FourDown() 
+        {
+            int[] result = new int[4];
+            for(int i = 0; i<4; i++)
+            {
+                int c = table.down();
+                result[i] = table.values[c];
+            }
+            return result;
+        }
+
+        public int[] FourUpLeft()
+        {
+            int myCursor = table.cursor;
+            int[] result = new int[4];
+            try{
+                for(int i=0; i<4; i++){
+                    myCursor;
+                    table.left();
+                    result[i] = table.values[]
+                
+                }
+            } catch {
+                throw new Exception();
+            }
+            return result;
+        } 
+    }*/
+}

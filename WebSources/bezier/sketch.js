5abe048780caec8fefe42bc7a8e811ae3022bbef
Lukas Marsch <50878285+LukasMarsch@users.noreply.github.com>
2022-11-18 15:40:18 +0100

    updated to display simple quadratic bezier curve

--- WebSources/bezier/sketch.js
+++ WebSources/bezier/sketch.js
@@ -10,10 +10,15 @@ function setup() {
 function draw() {
   var amount = 0;
   var v0 = createVector(0,0);
-  var v1 = createVector(400,200);
-  var v2 = createVector(100, 100);
+  var v1 = createVector(400,0);
+  var v2 = createVector(0,400);
   if(i < 0 || i > 1) step *= -1;
   i += step;
-  v3 = p5.Vector.lerp(v1,v2,i);
-  line(0,400, v3.x, v3.y);
+  v3 = lerpVector(v0, v1,i);
+  v4 = lerpVector(v2, v0, i);
+  line(v3.x, v3.y, v4.x, v4.y);
+}
+
+function lerpVector(v1, v2, amt){
+  return createVector(lerp(v1.x, v2.x, amt), lerp(v1.y, v2.y, amt));
 }

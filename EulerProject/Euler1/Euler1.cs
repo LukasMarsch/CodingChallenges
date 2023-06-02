9d40b5c3bd15fdeb2d297b5c31fa4f43a1d5f231
LukasMarsch <lmarsch@vater-gruppe.de>
2023-06-02 14:52:13 +0200

    Update Euler1.cs
    implemented additive algorithm without the need for hashset (it is fast!)

--- EulerProject/Euler1/Euler1.cs
+++ EulerProject/Euler1/Euler1.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 /* 
 Challenge:
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
@@ -27,28 +26,44 @@ We can also find all multiples by adding the factor to 0:
 ...
 this requires less iterations, in fact only 20-30% as many, butt:
 we have a whole bunch of other problems, like checking wether a number is a multiple of 3 and 5 at the same time
+
+The solution you see here is a custom algorithm consisting of two "bars" on the number scale
+    - i3 for all c -> c % 3 == 0
+    - i5 for all c -> c % 5 == 0
+
+we increase the i3 bar, and add all the multiples of 3 along the way
+we only increase the i5 bar, once it is at least 5 units from i3
+this way, if it is exactly 5 apart, we do not need to add i5 to sum, because we added it with i3 already.
+these numbers are multiples of 3 and 5 (15, 30, 45, ...)
+everything combined, we still O(n), butt significantly less comparisons (~25% less)
+so i take that as a w
  */
+ 
  class Euler1 {
     static void Main(String[] args) {
+        ushort n = 1000;
+        ushort i3 = 0;
+        ushort i5 = 0;
+        uint sum = 0;
+
+        
         DateTime start = DateTime.Now;
-        HashSet<Int32> list = new HashSet<Int32>();
-        foreach(int i in multiOfN(1000, 3)) {
-            list.Add(i);
-        }
-        foreach(int i in multiOfN(1000, 5)) {
-            list.Add(i);
+
+        while(i5 < n && i3 < n) {
+            sum += i3;
+            
+            if(i5 + 5 <= i3) {
+                i5 += 5;
+                if(i5 != i3) {
+                    sum += i5;
+                }
+            }
+            i3 += 3;
         }
 
-        int sum = list.Aggregate(1, (s, n) => s + n);
-        Console.WriteLine("Sum of all multiples of 3 and 5 till 1000 is: " + sum);
-        Console.WriteLine(DateTime.Now - start + " time");
+        Console.WriteLine("Time:  " + (DateTime.Now - start).ToString());
+        Console.WriteLine("Summe: " + sum);
         // correct answer is 233168    
-    }
-
-    static List<Int32> multiOfN(int size, int factor) {
-        List<Int32> multiples = new List<Int32>();
-        for(int i = factor; i < size; i+=factor) multiples.Add(i);
-        return multiples;
     }
 
 

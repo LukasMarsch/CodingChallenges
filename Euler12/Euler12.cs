36f255ed3f935e9351714f1e66909aa859b14729
LukasMarsch <lmarsch@vater-gruppe.de>
2023-06-14 10:54:02 +0200

    initial triangleNums

---
+++ Euler12/Euler12.cs
@@ -0,0 +1,20 @@
+﻿namespace Euler12;
+
+class Program
+{
+  public static void Main(String[] args)
+  {
+    List<uint> triangleNums = new List<uint>(1000);
+    uint i = 1;
+    triangleNums.Add(i);
+    while(i < 1000)
+    {
+      i++;
+      triangleNums.Add(i + triangleNums[triangleNums.Count - 1]);
+    }
+    foreach (var item in triangleNums)
+    {
+      Console.Write($"{item}, ");
+    }
+  }
+}

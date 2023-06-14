e73cbfb2f529d056aa69d964cabdcf6bb77538f3
LukasMarsch <lmarsch@vater-gruppe.de>
2023-06-14 13:14:06 +0200

    Add caching for known Divisors

--- Euler12/Euler12.cs
+++ Euler12/Euler12.cs
@@ -8,36 +8,70 @@
  */
 namespace Euler12;
 
+using System.Collections;
+
 class Program
 {
+  static Hashtable cache;
   public static void Main(String[] args)
   {
+    cache = new Hashtable();
     Stack<uint> triangleNums = new Stack<uint>();
-    // HashSet<Tuple<uint, List<uint>> = new HashSet<Tuple<uint, List<uint>>();
     uint i = 1;
     triangleNums.Push(i);
     uint numOfDivisors = 0;
-    while(numOfDivisors < 3 && i < 300)
+    while(numOfDivisors < 500)
     {
       i++;
       triangleNums.Push(i + triangleNums.Peek());
-      Console.WriteLine(getNumOfDivisors(triangleNums.Peek()));
-      Console.WriteLine(numOfDivisors);
+      numOfDivisors = getNumOfDivisors(triangleNums.Peek());
     }
     Console.Write($"{triangleNums.Pop()} -> {numOfDivisors} Divisors");
   }
 
   private static uint getNumOfDivisors(uint n) 
   {
-    Stack<uint> divisors = new Stack<uint>();
-    for (int i = 1; i < (int) Math.Floor((double)n/2); i++)
+    HashSet<uint> divisors = new HashSet<uint>();
+    for (uint i = (uint) Math.Floor((double)n/2); i > 0;  i--)
     {
-      if(i % n == 0)
+      if(n % i == 0)
       {
-        divisors.Push((uint)i);
-        Console.WriteLine(i);
+        try{
+          cache.Add(i, getDivisors(i));
+        }
+        catch(ArgumentException ex){
+          //Console.WriteLine(ex.ToString());
+        }
+        finally
+        {
+          divisors.UnionWith((IEnumerable<uint>) cache[i]);
+          divisors.Add(i);
+        }
       }
     }
-    return (uint)divisors.Count;
+    return (uint) divisors.Count;
+  }
+
+  private static HashSet<uint> getDivisors(uint n)
+  {
+    HashSet<uint> divisors = new HashSet<uint>((int)Math.Floor(Math.Sqrt(n)));
+
+    for(uint i = 1; i < (uint) Math.Floor(n/2.0); i++)
+    {
+      if( n % i == 0)
+      {
+        if(cache.ContainsKey(i))
+        {
+          divisors.UnionWith((IEnumerable<uint>) cache[i]);
+        }
+        else
+        {
+          cache.Add(i, getDivisors(i));
+          divisors.UnionWith((IEnumerable<uint>)cache[i]);
+        }
+        divisors.Add(i);
+      }
+    }
+    return divisors;
   }
 }

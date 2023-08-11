57f19f054da2ee3ba37aa9af7d3af380286087d3
Lukas Marsch <marschlukas@gmail.com>
2023-08-11 13:29:50 +0200

    working solution, but slow (~17sec)

--- Euler14/Euler14.clj
+++ Euler14/Euler14.clj
@@ -10,9 +10,18 @@
     (#(+ 1 (* % 3)) x)))
 
 (defn loop_me_daddy [x]
-  (loop [v ()]
-   (when (not (= 1 x))
-     (conj v x)
-     (recur (collatz x)))))
+  (loop [x x
+         v []]
+   (if (not= 1 x)
+     (recur (collatz x) (conj v x))
+     (count v))))
 
-(println (count (vector (loop_me_daddy 200))))
+(println
+  (loop [ci 1
+         maxi 0
+         maxc 0]
+    (if (< ci 1e6)
+      (if (> (loop_me_daddy ci) maxc)
+        (recur (inc ci) ci (loop_me_daddy ci))
+        (recur (inc ci) maxi maxc))
+      maxi)))

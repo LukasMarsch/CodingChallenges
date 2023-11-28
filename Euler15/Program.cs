e127c0b07e2f4fb2ae0e70209f4832d9c0803601
Lukas Marsch <marschlukas@gmail.com>
2023-11-28 07:12:48 +0100

    O(n^2) Ansatz, bitte mit Hashtable verbessern

--- Euler15/Program.cs
+++ Euler15/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Program;
+
+public static class Finder {
+    public static void Main() {
+        Console.WriteLine(Find(20, 20));
+    }
+
+    public static uint Find(uint x, uint y) => (x, y) switch {
+        ((uint) 1, _)      => (uint) y,
+        (_, (uint) 1)      => (uint) x,
+        _           => Find((uint) x-1, (uint) y) + Find((uint) x, (uint) y-1),
+    };
+
+}

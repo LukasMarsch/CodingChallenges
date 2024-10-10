8f705f7efff0c3e12e76777e4a886e164d6d5d18
lmarsch <marschlukas@gmail.com>
2024-10-10 10:58:48 +0200

    Euler 16

---
+++ Euler16/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+
+public class Program {
+    public static void Main(string[] args) {
+        BigInteger d = System.Numerics.BigInteger.Pow(new BigInteger(2), 1000);
+        BigInteger ten = new BigInteger(10);
+        BigInteger sum = 0;
+        BigInteger remainder = 0;
+        while(d >= 1) {
+            d = BigInteger.DivRem(d, ten, out remainder);
+            sum += remainder;
+        }
+
+        Console.Write(sum);
+    }
+}

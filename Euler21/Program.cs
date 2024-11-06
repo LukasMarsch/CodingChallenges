f5b5005df9333155dfe814389c3787b308e56e05
lmarsch <marschlukas@gmail.com>
2024-11-06 13:22:32 +0100

    Euler21

---
+++ Euler21/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace amicable;
+public class Program {
+    public static void Main(string[] Args) {
+        Dictionary<int, int> numbers = new Dictionary<int, int>();
+        int n = 2;
+        while(n < 10000) {
+            int p = SumOfDivisors(n);
+            if(p > 1)
+                numbers.Add(n, p);
+            n++;
+        } 
+        Console.WriteLine(SumOfAmicableNumbers(numbers));
+    }
+
+    private static int SumOfDivisors(int n) =>
+        Enumerable.Range(1, n/2).Where(x => n % x == 0 && x != n).Sum();
+
+    private static int SumOfAmicableNumbers(Dictionary<int, int> numbers) {
+        HashSet<int> sum = new HashSet<int>();
+        int temp;
+        foreach(var valuePair in numbers) {
+            if(numbers.TryGetValue(valuePair.Value, out temp)) {
+                if(temp == valuePair.Key && valuePair.Value != temp) {
+                    sum.Add(valuePair.Key);
+                }
+            }
+        }
+        foreach(var i in sum)
+            Console.WriteLine(i);
+        return sum.Sum();
+    }
+}
---
+++ Euler21/Program1.cs
@@ -0,0 +1,32 @@
+﻿namespace amicable;
+public class Program {
+    public static void Main(string[] Args) {
+        Dictionary<int, List<int>> numbers = new Dictionary<int, List<int>>();
+        int n = 2;
+        List<int> temp = new List<int>(0);
+        while(n < 10000) {
+            int p = SumOfDivisors(n);
+            if(numbers.TryGetValue(p, out temp)) {
+                temp.Add(n);
+            } else {
+                numbers.Add(p, new List<int>(){n});
+            }
+            n++;
+        } 
+        Console.WriteLine(SumOfAmicableNumbers(numbers));
+    }
+
+    private static int SumOfDivisors(int n) =>
+        Enumerable.Range(1, n-1).Where(x => n % x == 0 && x != n).Sum();
+
+    private static int SumOfAmicableNumbers(Dictionary<int, List<int>> numbers) {
+        int sum = 0;
+        foreach(var vp in numbers) {
+            Console.WriteLine($"{vp.Key}: {vp.Value.ToString()}");
+            if(vp.Value.Count() > 1) {
+                sum += vp.Key;
+            }
+        }
+        return sum;
+    }
+}
